42c16e3d66376fb66201fae6d84f1e4db4692ce4
waiscodes <b.wais@outlook.com>
2021-01-13 15:57:16 -0700

    one way data binding

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -3,26 +3,25 @@
     <p>Hello World</p>
     <p v-once>My name is {{ myName }}</p>
     <p>My name is {{ myName }}</p>
-    <p v-html="rawHTML"></p>
-    <p>raw HTML: {{ rawHTML }}</p>
-    <!-- Be careful with this method. Suspetible to cross site scripting -->
+    <input :value="randomNumber" />
+    <button @click="changeNum">Change Number</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    // Data is a method that contains all the data that goes on the template
     return {
       myName: "Birm Wais",
-      rawHTML: "<strong>raw HTML</strong>",
+      randomNumber: 5,
     };
   },
   methods: {
-    // This is just a regular object not a function like methods
-    // You define your methods inside this object
     helloWorld() {
       alert("Something");
+    },
+    changeNum() {
+      this.randomNumber = 100;
     },
   },
   mounted() {
