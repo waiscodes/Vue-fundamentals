67e123dca5795d5a568bb7416e98474e6e288303
waiscodes <b.wais@outlook.com>
2021-01-13 14:00:36 -0700

    added a data method and rendered it on template

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <p>Hello World</p>
+    <p>My name is {{ myName }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    // Data is a method that contains all the data that goes on the template
+    return {
+      myName: "Birm Wais",
+    };
+  },
+};
 </script>
 
 <style scoped></style>
